b1b68dc21329dbcdf643039fac028fa5281a7b0c
Hashwolf_Swarup <75833762+Swarup-hash@users.noreply.github.com>
2023-06-26 02:15:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,49 @@
-const btnLeft = document.querySelector(".left-btn");
+// Navbar
 
-const btnRight = document.querySelector(".right-btn");
+const tabsBox = document.querySelector(".tabs-box"),
+allTabs = tabsBox.querySelectorAll(".tab"),
+arrowIcons = document.querySelectorAll(".icon i");
 
-const tabMenu = document.querySelector(".tab-menu");
+let isDragging = false;
 
-const IconVisibility = () => {
-    let scrollLeftValue = Math.ceil(tabMenu.scrollLeft);
-    console.log("1.", scrollLeftValue);
-    let scrollableWidth = tabMenu.scrollwidth - tabMenu.clientWidth;
-    console.log("2.", scrollableWidth);
-
-
-    btnLeft.style.display = scrollLeftValue > 0 ? "block": "none"; 
-    btnRight.style.display = scrollablewidth > scrollLeftValue ? "block" : "none"
-
+const handleIcons = (scrollVal) => {
+    let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
+    arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? "none" : "flex";
+    arrowIcons[1].parentElement.style.display = maxScrollableWidth - scrollVal <= 1 ? "none" : "flex";
 }
 
-btnRight.addEventListener("click", () => {
-tabMenu.scrollLeft += 150;
-//IconVisibility();
-setTimeout(() => IconVisibility(), 50);
-
-});
-
-btnLeft.addEventListener("click", () => {
-tabMenu.scrollLeft -= 150;
-//IconVisibility();
-setTimeout(() => IconVisibility(), 50);
-});
-
-window.onload = function(){
-    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none"
-    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
-}
-
-window.onresize = function(){
-    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none"
-    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
-
-    let scrollLeftValue = Math.round(tabMenu.scrollLeft);
-    btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
-}
-
-let activeDrag = false;
-
-tabMenu.addEventListener("mousemove", (drag) => { 
-    if(!activeDrag) return; 
-    tabMenu.scrollLeft -= drag.movementX;
-    IconVisibility();
-   
-
-});
-
-document.addEventListener("mouseup", () => {
-    activeDrag = false;
-   
-
-})
-
-tabMenu.addEventListener("mousedown", () => { 
-    activeDrag = true; 
-});
-
-
-const tabs = document.querySelectorAll(".tab"); 
-const tabBtns= document.querySelectorAll(".tab-btn");
-
-const tab_Nav = function(tabBtnClick){
-    tabBtns.forEach((tabBtn) => {
-        tabBtn.classList.remove("active");
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        // if clicked icon is left, reduce 350 from tabsBox scrollLeft else add
+        let scrollWidth = tabsBox.scrollLeft += icon.id === "left" ? -340 : 340;
+        handleIcons(scrollWidth);
     });
-
-    tabs.forEach((tab) =>{
-        tab.classList.remove("active");
-    })
-
-tabBtns[tabBtnClick].classList.add("active"); 
-tabs[tabBtnClick].classList.add("active"); }
-
-tabBtns.forEach((tabBtn, i) => { tabBtn.addEventListener("click", () => {
-
-tab_Nav(i);
-
 });
 
+allTabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabsBox.querySelector(".active").classList.remove("active");
+        tab.classList.add("active");
+    });
 });
 
+const dragging = (e) => {
+    if(!isDragging) return;
+    tabsBox.classList.add("dragging");
+    tabsBox.scrollLeft -= e.movementX;
+    handleIcons(tabsBox.scrollLeft)
+}
 
+const dragStop = () => {
+    isDragging = false;
+    tabsBox.classList.remove("dragging");
+}
 
+tabsBox.addEventListener("mousedown", () => isDragging = true);
+tabsBox.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+//Carousle 
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -123,3 +79,26 @@ function preLoad(){
 }
 
 //Slider
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
