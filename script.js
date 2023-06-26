a78fcd53aa86035457f03ea550fd28fc5debbdb2
Hashwolf_Swarup <75833762+Swarup-hash@users.noreply.github.com>
2023-06-26 15:34:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,6 +71,8 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
+
 //Loader
 
 const preLoader=document.querySelector("#loading");
@@ -100,5 +102,80 @@ $(document).ready(function(){
                 slidesToShow: 3
             }
         }]
+    });
+});
+
+//SLiding Card
+
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+$(document).ready(function() {
+    $("#news-slider1").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+$(document).ready(function() {
+    $("#news-slider2").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,4],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,    
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+$(document).ready(function() {
+    $("#news-slider3").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,4],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,    
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+$(document).ready(function() {
+    $("#news-slider4").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,4],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,    
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+$(document).ready(function() {
+    $("#news-slider5").owlCarousel({
+        items : 4,
+        itemsDesktop:[1199,4],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,    
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
     });
 });
